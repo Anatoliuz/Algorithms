e1ba7f3304a85f5aac3a87d396249955a210fe66
Anatoliuz <afilinovs@gmail.com>
2015-12-16 00:01:40 +0300

    Binary Tree Search Done

--- main.cpp
+++ main.cpp
@@ -11,44 +11,45 @@
 #define MAX 6
 #define INF 100000000
 
-//void Merge(int *A, int p, int q, int r)
-//{
-//    int n1 = q - p + 1;
-//    int n2 = r - q;
-//    int L[ n1 + 1];
-//    int R[ n2 + 1];
-//    memset(L, 0, sizeof(L));
-//    memset(R, 0, sizeof(R));
-//    for (int i = 0; i < n1; ++i) {
-//        L[i] = A[ p + i - 1];
-//    }
-//    for (int j = 0; j < n2; ++j)
-//    {
-//        R[j] = A[q + j];
-//    }
-//    L[ n1 ] = INF;
-//    R[ n2 ] = INF;
-//    int i = 1;
-//    int j = 1;
-//    for (int k = p; k < r; ++k) {
-//        if (L[i] <= R[j]) {
-//            A[k] = L[i];
-//            ++i;
-//        }
-//        else{
-//            A[k] = R[j];
-//            j++;
-//        }
-//    }
-//}
-//void MergeSort(int* A, int p, int r)
-//{
-//    if ( p < r)
-//    {
-//        int q = (p + r) / 2;
-//        
-//    }
-//}
+void Merge(int *A, int p, int q, int r)
+{
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int L[ n1 + 1 ];
+    int R[ n2 + 1 ];
+    for (int i = 0; i <= n1; ++i) {
+        L[i] = A[ p + i - 1];
+    }
+    for (int j = 0; j <= n2; ++j)
+    {
+        R[j] = A[q + j];
+    }
+    L[ n1 + 1 ] = INF;
+    R[ n2 + 1 ] = INF;
+
+    int i = 0;
+    int j = 0;
+    for (int k = p; k < r; ++k) {
+        if (L[i] <= R[j]) {
+            A[k] = L[i];
+            ++i;
+        }
+        else{
+            A[k] = R[j];
+            j++;
+        }
+    }
+}
+void MergeSort(int* A, int p, int r)
+{
+    if ( p < r)
+    {
+        int q = (p + r)/2;
+        MergeSort(A, p, q);
+        MergeSort(A, q + 1, r);
+        Merge(A, p, q, r);
+    }
+}
 
 
 
@@ -78,8 +79,25 @@ public:
                 tree -> right = Insert(tree->right, data);
             }
         return tree;
+        }
     }
-    }
+        Tree* Search(Tree* tree, int data){
+            if(tree != nullptr){
+                if (tree->data == data ) {
+                    return tree;
+                }
+                else if (data < tree->data)
+                {
+                    return Search(tree->left, data);
+            
+                }
+                 else if (data > tree->data)
+                {
+                    return Search(tree->right, data);
+                }
+            }
+             return NULL;
+        }
     void Print(Tree* tree){
         while (tree != 0) {
             std::cout << tree -> data;
@@ -96,10 +114,10 @@ int main(int argc, const char * argv[])
 {
     Tree* tree;
     tree = tree -> NewTree(10);
-    for(auto n: { 9, 8 , 7}){
+    for(auto n: { 9, 18 , 7, 4}){
       tree->Insert(tree, n);
     }
-    
+    bool a = tree->Search(tree, 4);
    // tree -> Print(tree);
     return 0;
 }
